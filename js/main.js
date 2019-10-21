a64891f8e9557911f06de8fb2cb7ab9aafa54049
Igor Polonskiy-Buslaev <barlogfire@ya.ru>
2019-10-21 22:57:52 +0300

    stack section in work

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+
 $(window).scroll(function(){
+
   var docscroll=$(document).scrollTop();
-  if(docscroll>$('nav').height()){
-  	$('nav').addClass('.blackbag');
+  if(docscroll>$('nav').height()|| $('nav').height()>105 ){
+  	$('nav').addClass('blackbag');
   }else{
-    $('nav').removeClass('.blackbag');
+    $('nav').removeClass('blackbag');
  }
 });
